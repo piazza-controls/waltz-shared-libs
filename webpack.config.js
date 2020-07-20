36f2a3edea8f5c214fef65dfc84dae39b9eb431b
chernov <kapot65@gmail.com>
2020-07-20 23:30:34 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        vendor: ['react', 'react-dom', 'redux', 'react-redux', '@reduxjs/toolkit'],
+    },
+    mode: "production",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js",
+        library: "[name]"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            }
+        ]
+    },
+    devtool: false,
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, "dist", "[name]-manifest.json"),
+            format: true,
+            name: "[name]"
+        })
+    ]
+};
